08e036be4039314adea51517c5629abe5a92a30b
justinbusker <jtbusker@icloud.com>
2025-04-24 18:31:51 -0500

    motor driver code

--- motors.py
+++ motors.py
@@ -2,32 +2,29 @@ import RPi.GPIO as GPIO
 import time
 
 # Pin Definitions
-ENB = 2
-IN3 = 3
+IN3 = 17
+IN4 = 27
+ENA = 22
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # Setup
-motor_pins = [ENA, IN1, IN2, ENB, IN3, IN4]
-for pin in motor_pins:
-    GPIO.setup(pin, GPIO.OUT)
+GPIO.setup([IN3, ENA, IN4], GPIO.OUT)
 
-# Optional: Enable PWM for speed control
-pwm_b = GPIO.PWM(ENB, 100)
-pwm_a.start(100)  # Full speed
-pwm_b.start(100)
 
 # Forward
-GPIO.output(IN3, GPIO.HIGH)
-GPIO.output(IN4, GPIO.LOW)
 
-time.sleep(2)
+pwm = GPIO.PWM(ENA, 100)
+pwm.start(25)
+
+GPIO.output(IN4, GPIO.LOW)
+GPIO.output(IN3, GPIO.HIGH)
+print("pin set to high")
+time.sleep(20)
 
 # Stop
 GPIO.output(IN3, GPIO.LOW)
-GPIO.output(IN4, GPIO.LOW)
 
-pwm_a.stop()
-pwm_b.stop()
+pwm.stop()
 GPIO.cleanup()
